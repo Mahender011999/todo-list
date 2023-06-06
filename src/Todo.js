8ded991ccc2aa1570b25dd1c5d912b11d4bce08b
mahender011999 <mahender011999@gmail.com>
2023-06-06 17:07:59 +0530

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react';
+
+function Todo() {
+    const [list,setList] = useState([])
+    const [message,setMessage] = useState({
+        text:'',
+        id:'',
+    })
+    const changeHandel = (e)=>{
+           setMessage({...message,text:e.target.value})
+    }
+    const submitMe =(e) =>{
+         e.preventDefault();
+         let newTodo={
+          text:message.text,
+          id:new Date().getTime().toString(),
+         };
+         setList([...list,newTodo])
+         setMessage({
+          text:'',
+          id:''
+         })
+      }
+
+      const handelDelete =(id)=>{
+        let newTodos = list.filter((eachItem)=>{
+          return eachItem.id !==id
+        })
+        setList(newTodos)
+      }
+  return (
+    <center>
+      <form onSubmit={submitMe}>
+      <input type='text' name='text' id='text'  placeholder='Enter Your Text' 
+      value={message.text} onChange={changeHandel}  />
+      <button type='submit'>Add</button>
+      <hr/>
+      {
+       list.length === 0 && <h1>There is no items </h1>
+
+      }
+      <ul>
+      {list.map((eachItem)=>{
+        const {id,text} = eachItem
+        return <li key={id}>
+             <span>{text}</span>
+             <button>Edit</button>
+             <button onClick={()=>handelDelete(id)}>Delete</button>
+        </li>
+      })}
+      </ul>
+      
+      </form>
+    </center>
+  )
+}
+
+export default Todo
